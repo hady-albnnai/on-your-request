7813bee1c5cedb44b154ea8874b9c7c6b52347b4
Hady <Saker.aysami@gmail.com>
2026-05-28 23:55:21 +0300

    Add basalt400 color

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -10,6 +10,7 @@ abstract class AppColors {
   static const basalt600 = Color(0xFF4F4A42);
   static const basalt500 = Color(0xFF6B6560);
   static const basalt300 = Color(0xFF9E9890);
+  static const basalt400 = Color(0xFFB5AFA8);
   static const basalt100 = Color(0xFFD6D0C8);
   static const basalt50  = Color(0xFFEDE9E2);
 
