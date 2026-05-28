222339be13d2a8e7131cf551e2c2d086721c98b8
Hady <Saker.aysami@gmail.com>
2026-05-28 23:40:40 +0300

    Add AppRoutes – navigation routes

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,11 @@
+/// مسارات التنقل في تطبيق "بخدمتك"
+abstract class AppRoutes {
+  static const splash     = '/';
+  static const welcome    = '/welcome';
+  static const phoneAuth  = '/phone-auth';
+  static const otp        = '/otp';
+  static const home       = '/home';
+  static const addPost    = '/add-post';
+  static const postDetail = '/post-detail';
+  static const myAccount  = '/my-account';
+}
